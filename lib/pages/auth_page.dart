851aebf513eec238e533c79643e6bfd145779a3f
Joao Felipe <jfcbxp@gmail.com>
2022-11-02 08:54:45 -0300

    adicionado camada de mock service

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:chat/components/auth_form.dart';
 import 'package:chat/models/auth_form_data.dart';
+import 'package:chat/services/auth/auth_mock_service.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -16,7 +17,12 @@ class _AuthPageState extends State<AuthPage> {
     try {
       setState(() => _isLoading = true);
 
-      if (formData.isLogin) {}
+      if (formData.isLogin) {
+        await AuthMockService().login(formData.email, formData.password);
+      } else {
+        await AuthMockService().signup(
+            formData.name, formData.email, formData.password, formData.image);
+      }
     } catch (error) {
     } finally {
       setState(() => _isLoading = false);
--- lib/services/auth/auth_mock_service.dart
+++ lib/services/auth/auth_mock_service.dart
@@ -1,32 +1,58 @@
+import 'dart:math';
+
 import 'package:chat/models/chat_user.dart';
 import 'dart:io';
+import 'dart:async';
 
 import 'package:chat/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+
+    controller.add(_currentUser);
+  });
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(user);
+  }
+
   @override
   // TODO: implement currentUser
-  ChatUser? get currentUser => throw UnimplementedError();
-
-  @override
-  Future<void> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  ChatUser? get currentUser {
+    return _currentUser;
   }
 
   @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<void> login(String email, String password) async {
+    _updateUser(_users[email]);
   }
 
   @override
-  Future<void> signup(String nome, String email, String password, File image) {
-    // TODO: implement signup
-    throw UnimplementedError();
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  @override
+  Future<void> signup(
+      String nome, String email, String password, File? image) async {
+    final newUser = ChatUser(
+        id: Random().nextDouble().toString(),
+        name: nome,
+        email: email,
+        imageUrl: image?.path ?? '/assets/images');
+
+    _users.putIfAbsent(email, () => newUser);
+
+    _updateUser(newUser);
   }
 
   @override
   // TODO: implement userChanges
-  Stream<ChatUser?> get userChanges => throw UnimplementedError();
+  Stream<ChatUser?> get userChanges => _userStream;
 }
