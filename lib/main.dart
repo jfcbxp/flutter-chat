8d2423fd9e37c8e67d9dab5a9425f785b8d0c5d2
Joao Felipe <jfcbxp@gmail.com>
2022-11-24 10:04:50 -0300

    adicionado pagina de notificacoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:chat/pages/auth_or_app_page.dart';
+import 'package:chat/services/notification/chat_notification_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +11,18 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatNotificationService())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Chat',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: AuthOrAppPage(),
       ),
-      home: AuthOrAppPage(),
     );
   }
 }
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,12 @@
+import 'dart:math';
 import 'package:chat/components/messages.dart';
 import 'package:chat/components/new_message.dart';
+import 'package:chat/models/chat_notification.dart';
+import 'package:chat/pages/notification_page.dart';
 import 'package:chat/services/auth/auth_service.dart';
+import 'package:chat/services/notification/chat_notification_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -12,38 +17,73 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Chat'),
         actions: [
-          DropdownButton(
-              icon: Icon(
-                Icons.more_vert,
-                color: Theme.of(context).primaryIconTheme.color,
-              ),
-              items: [
-                DropdownMenuItem(
-                    value: 'Logout',
-                    child: Container(
-                      child: Row(children: [
-                        Icon(
-                          Icons.exit_to_app,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text('Sair')
-                      ]),
-                    ))
-              ],
-              onChanged: (value) {
-                if ((value) == 'logout') {
-                  AuthService().logout();
-                }
-              })
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+                icon: Icon(
+                  Icons.more_vert,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                items: [
+                  DropdownMenuItem(
+                      value: 'Logout',
+                      child: Container(
+                        child: Row(children: [
+                          Icon(
+                            Icons.exit_to_app,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('Sair')
+                        ]),
+                      ))
+                ],
+                onChanged: (value) {
+                  if ((value) == 'logout') {
+                    AuthService().logout();
+                  }
+                }),
+          ),
+          Stack(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (ctx) {
+                      return NotificationPage();
+                    }));
+                  },
+                  icon: Icon(Icons.notifications)),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  maxRadius: 10,
+                  backgroundColor: Colors.red.shade800,
+                  child: Text(
+                    '${Provider.of<ChatNotificationService>(context).itemsCount}',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
       body: SafeArea(
         child: Column(
           children: [Expanded(child: Messages()), NewMessage()],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Provider.of<ChatNotificationService>(context, listen: false).add(
+              ChatNotification(
+                  title: Random().nextDouble().toString(),
+                  body: Random().nextDouble().toString()));
+        },
       ),
     );
   }
